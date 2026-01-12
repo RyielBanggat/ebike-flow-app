ec373017f4975cdfc62b8d3dea4b76decc2932df
Ryiel Banggat <ryiel.banggat@jmc.edu.ph>
2026-01-12 16:57:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-507371065-bfbae",
+  "appId": "1:1084353730938:web:967be2a00b7439c2dcdec3",
+  "apiKey": "AIzaSyADSXpZsPb6hUcW22NwaIfjxBaFjRf2gpo",
+  "authDomain": "studio-507371065-bfbae.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1084353730938"
+};
